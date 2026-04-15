2cf7712da7a28f3b24317c37c98b95702ee60fb2
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-15 15:26:32 -0300

    entendendo operador ternario

---
+++ operador_ternario.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo PHP</title>
+</head>
+<body>
+    <?php
+       $nota = 10; //lembra do ';'
+
+       $nota <= 10 ? 'Reprovado' : 'aprovado';
+    ?> 
+</body>
+</html>
