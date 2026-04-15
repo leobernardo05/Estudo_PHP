985e9b88582a857d9f3e20a920e1df9f12b9fd68
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-15 11:59:17 -0300

    Create concatenacao.php
    entendendo a estrutura de concatenação no php.

---
+++ concatenacao.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo PHP</title>
+</head>
+<body>
+    <?php
+        $nome = 'Léo';
+        $idade = '19';
+        $hobbie = 'nadar';
+        // operador .
+       echo 'Olá ' . $nome . ', você tem ' . $idade . ' anos e gosta de ' . $hobbie . '.';
+    ?> 
+</body>
+</html>
