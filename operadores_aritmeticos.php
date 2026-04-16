e4ebce67d217e9a2571effa39f338b2c8df93a04
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-16 15:55:50 -0300

    Create operadores_aritmeticos.php

---
+++ operadores_aritmeticos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo PHP</title>
+</head>
+<body>
+    <?php
+       $num1 = 13;
+       $num2 = 2;
+       echo "A soma entre $num1 e $num2 é: " . ($num1 + $num2) . '<br>';
+       echo "A divisão entre $num1 e $num2 é: " . ($num1 / $num2) . '<br>' ;
+       echo "A multiplicação entre $num1 e $num2 é: " . ($num1 * $num2) . '<br>';
+       echo "A subtração entre $num1 e $num2 é: " . ($num1 - $num2) . '<br>';
+       echo "O modulo entre $num1 e $num2 é: " . ($num1 % $num2) . '<br>';
+
+
+    ?> 
+</body>
+</html>
