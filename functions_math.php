1b5e900e70e6bd5ffb7a94a4b3aaf7f33e64ca55
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-17 14:43:36 -0300

    update functions_math.php

--- functions_math.php
+++ functions_math.php
@@ -7,7 +7,18 @@
 </head>
 <body>
     <?php
-        
+        $numero = 10.5;
+        //-------- Funções nativas para tarefas matemáticas ----------
+        // ceil($numero) -> Arredonda o valor para cima
+        echo ceil($numero) . '<br>';
+        // floor($numero) -> Arredonda o valor para baixo
+        echo floor($numero) . '<br>';
+        // round($numero) -> Arredonda o valor com base nas casas decimais
+        echo round($numero) . '<br>';
+        // rand() -> gera um inteiro aleatorio
+        echo rand() . '<br>';
+        // sqrt($numero) -> retorna a raiz quadrada
+        echo sqrt($numero) . '<br>';
     ?> 
 </body>
 </html>
