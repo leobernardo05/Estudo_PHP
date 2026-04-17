3e28f249c026a2913bc53eaa76952b53502fbc30
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-17 14:11:05 -0300

    Create functions_manipular_strings.php

---
+++ functions_manipular_strings.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo PHP</title>
+</head>
+<body>
+    <?php
+        /*
+            strtolower($texto) --> Todas as letras em minusculas
+            strtoupper($texto) --> Todas as letras em maiusculo
+            ucfirst($texto) --> Transforma o primeiro caracter em string maiúscula
+            strlen($texto) --> Conta a quantidade de caracteres 
+            str_replace(<procura por>, <substitui por>, $texto) --> Substitui uma cadeia de caracteres por outra dentro da string
+            substr($texto, <posicao inicial>,<qtde caracteres>) --> retorna parte de uma string
+        */ 
+    ?> 
+</body>
+</html>
