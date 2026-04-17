667f476eb1dd233a7bb7e4fffc048871a8b41b41
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-17 15:26:56 -0300

    update

--- functions_data.php
+++ functions_data.php
@@ -8,9 +8,25 @@
 <body>
     <?php
         // date(formato) -> Recuperar a data atual
+        echo date('d/m/y H:i:s'); // -> visualizar documentação PHP
         // date_default_timezone_get(timezone) -> Recuperar o timezone default da aplicação
+        echo '<br />';
+        echo date_default_timezone_get();
         // date_default_timezone_set(timezone) -> Atualizar o timezone default da aplicação
+        date_default_timezone_set('America/Sao_Paulo');
+        echo '<br />';
+        echo date('d/m/Y H:i');
+        echo '<br />';
+
+        $data_inicial = '2018-04-24';
+        $data_final = '2018-05-15';
         // strtotime(data) -> Transformar datas textuais em segundas
+        $time_inicial = strtotime($data_inicial);
+        $time_final = strtotime($data_final);
+        echo $data_inicial . ' - ' . $time_inicial;
+        echo '<br />';
+        echo $data_final . ' - ' . $time_final;     
+
     ?> 
 </body>
 </html>
