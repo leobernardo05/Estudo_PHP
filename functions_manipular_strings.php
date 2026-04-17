cb4ba967fd6c8ccfa25c7d08d3d48a7c653a61d7
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-17 14:34:28 -0300

    update

--- functions_manipular_strings.php
+++ functions_manipular_strings.php
@@ -19,7 +19,7 @@
             // str_replace(<procura por>, <substitui por>, $texto) --> Substitui uma cadeia de caracteres por outra dentro da string
             echo str_replace('Curso', 'Aulas', $texto) . '<br>'; //--> substitui Curso por Aulas
             // substr($texto, <posicao inicial>,<qtde caracteres>) --> retorna parte de uma string
-            echo substr($texto) . '<br>';
+            echo substr($texto, 1, 13) . '<br>';
     ?> 
 </body>
 </html>
