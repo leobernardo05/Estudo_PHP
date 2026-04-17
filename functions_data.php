621aa0f850724d074d7a30876b67242c4ffe3727
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-17 14:52:54 -0300

    Create functions_data.php

---
+++ functions_data.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo PHP</title>
+</head>
+<body>
+    <?php
+        // date(formato) -> Recuperar a data atual
+        // date_default_timezone_get(timezone) -> Recuperar o timezone default da aplicação
+        // date_default_timezone_set(timezone) -> Atualizar o timezone default da aplicação
+        // strtotime(data) -> Transformar datas textuais em segundas
+    ?> 
+</body>
+</html>
