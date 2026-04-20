ac1a3d365caf0e32039da963bef3d2a6f3dd5d65
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-20 11:23:59 -0300

    Create array_multidimensional.php

---
+++ array_multidimensional.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo PHP</title>
+</head>
+<body>
+    <?php
+        // $lista_coisas = array();
+        $lista_coisas = [];
+
+        $lista_coisas['frutas'] = array('Banana', 'Maça', 'Morango', 'Uva'); //array bidimensional -> em listas coisas está contido as frutas
+        $lista_coisas['pessoas'] = ['João', 'José', 'Maria'];
+
+        echo '<pre>';
+        print_r($lista_coisas);
+        echo '<pre>';
+    ?> 
+</body>
+</html>
