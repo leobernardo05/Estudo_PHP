f7af8dff4ab6018dc6298888e650e1e7f13a6214
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-20 12:09:14 -0300

    update

--- array_pesquisa.php
+++ array_pesquisa.php
@@ -7,7 +7,26 @@
 </head>
 <body>
     <?php
+        // in_array()
+        // array_search() ---> retorna o índice do valor pesquisado, caso ele exista 
+        $lista_frutas = ['Banana', 'Maça', 'Morango', 'Uva'];
+
+        echo '<pre>';
+        print_r($lista_frutas);
+        echo '</pre>';
+
+        //$existe = in_array('Abacate', $lista_frutas);
+        // true -> texto = 1
+        // false -> texto = vazio
+        echo array_search('Uva', $lista_frutas);
+        // null
         
+        /*
+            if($existe){ //true / false
+                echo 'Sim, o valor pesquisado existe no array';
+            } else {
+                echo 'Não, o valor pesquisado existe no array';
+        }*/
     ?> 
 </body>
 </html>
