18e7d5349138823ab0dee14362cfefd530af91da
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-20 10:39:13 -0300

    Create array_basico.php

---
+++ array_basico.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estudo PHP</title>
+</head>
+<body>
+    <?php
+        //sequenciais (númericos)
+        $lista_frutas = array('Banana', 'Maça', 'Morango', 'Uva');
+        $lista_frutas[] = 'Abacaxi'
+
+        echo '<pre>';
+            var_dump($lista_frutas);
+        echo '<hr />';
+            print_r($lista_frutas);
+        echo '<pre>';
+
+        // associativos
+    ?> 
+</body>
+</html>
