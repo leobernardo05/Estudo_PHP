139913898694bf3cceb4479fa4ddba5c3c938863
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-20 15:54:06 -0300

    update

--- false_null_empty.php
+++ false_null_empty.php
@@ -7,6 +7,35 @@
 </head>
 <body>
     <?php
+        // false (true/false) - tipo variavel boolean
+        // null e empty - valores especiais
+
+        $funcionario1 = null;
+        $funcionario2 = '';
+        $funcionario3 = false;
+
+        // valores vazios
+        if (is_null($funcionario2)) {
+            echo 'Sim, a variavel é null';
+        } else {
+            echo 'Não, a variavel não é util';
+        }
+
+        echo '<br>';
+        // valores nulls
+        if (is_null($funcionario1)) {
+            echo 'Sim, a variavel é null';
+        } else {
+            echo 'Não, a variavel não é util';
+        }
+
+        echo '<br>';
+
+        if(is_null($funcionario3)){
+            echo 'Sim, a variavel é null';
+        } else {
+            echo 'Não, a variavel não é util';
+        }
         
     ?> 
 </body>
