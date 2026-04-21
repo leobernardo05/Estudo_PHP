45ce424b3e999cff4b58474d4750cf088ed336e2
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-21 17:17:12 -0300

    update

--- loops_pratica2.php
+++ loops_pratica2.php
@@ -7,41 +7,19 @@
 </head>
 <body>
     <?php
-       $array = ['1','2','3','4','3','2', '1'];
-        $i = 0;
-        //while = enquanto
-        while ( $i < count($array) ) {
-            echo $array[$i] . "<br>";
-            $i++;
-        }
-        
-        // laço do-while --> faça-equanto
-        $i = 0;
+      $funcionarios = array(
+        array('nome' => 'João', 'salario' => 2500), 
+        array('nome' => 'Maria', 'salrio' => 3000), 
+        array('nome' => 'Júlia', 'salario' => 4500)
+      );
 
-        do {
-            echo $array[$i] . "<br>";
-            $i++;
-        } while ( $i < count($array));
+      echo '<pre>';
+        print_r($funcionarios);
+      echo '</pre>';
 
-        // laço for -> mais usado
-        for($j = 0; $j < count($array); $j++){
-            echo $array[$j] . "<br>";
-        }
-
-        // forech -- laço de repetição
-        $itens = array('sofa', 'mesa', 'cadeira', 'fogao', 'geladeira');
-        echo '<pre>';
-            print_r($itens);
-        echo '</pre>';
-
-        foreach($itens as $item) {
-            echo "$item <br/>";
-
-            if($item == 'mesa') {
-                echo '(Compre uma mesa e ganhe 25% de desconto em uma geladeira)';
-            }
-        }
-
+      foreach($funcionarios as $idx => $nome_funcionario) {
+        echo $nome_funcionario . '<br>';
+      }
     ?> 
 </body>
 </html>
