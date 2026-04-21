a3c65b03d589b22c894f2253975a3c511f20e296
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-21 16:51:27 -0300

    update

--- lacos_rep.php
+++ lacos_rep.php
@@ -7,7 +7,26 @@
 </head>
 <body>
     <?php
-       
+       $array = ['1','2','3','4','3','2', '1'];
+        $i = 0;
+        //while = enquanto
+        while ( $i <= count($array) ) {
+            echo $array[$i] . "<br>";
+            $i++;
+        }
+        
+        // laço do-while --> faça-equanto
+        $i = 0;
+
+        do {
+            echo $array[$i] . "<br>";
+            $i++;
+        } while ( $i <= $array.count())
+
+        // laço for -> mais usado
+        for($j = 0; $j <= $array.count(); j++){
+            echo $array[$j] . "<br>";
+        }
     ?> 
 </body>
 </html>
