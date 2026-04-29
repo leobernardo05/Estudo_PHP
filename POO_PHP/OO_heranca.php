05052ee7ffa338a22ca763087f4b2924ca92d668
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-29 15:49:53 -0300

    update

--- POO_PHP/OO_heranca.php
+++ POO_PHP/OO_heranca.php
@@ -1,3 +1,66 @@
 <?php
-   
+    class Veiculo {
+        public $placa = null;
+        public $cor = null;
+
+        function acelerar(){
+            echo 'Acelerar';
+        }
+
+        function frear() {
+            echo 'freando';
+        }
+    }
+
+    class Carro extends Veiculo { //pilar da herança aqui
+        public $teto_solar = true;
+
+        function __construct($placa, $cor) {
+            $this->placa = $placa;
+            $this->cor = $cor;
+        }
+
+        function abrirTetoSolar () {
+            echo 'Abrir teto solar';
+        }
+
+        function alterarPosicaoVolante(){
+            echo 'Alterar posição volante';
+        }
+    }
+
+    class Moto extends Veiculo { //pilar da herança aqui
+        public $contraPesoGuiado = true;
+
+        function __construct($placa, $cor) {
+            $this->placa = $placa;
+            $this->cor = $cor;
+        }
+
+        function empinar(){
+            echo 'empinar';
+        }
+    }
+
+    $carro = new Carro('ABC1234', 'Branco');
+    $moto = new Moto('ABC1E22', 'Preta');
+
+    echo '<pre>';
+        print_r($carro);
+    echo '</pre>';
+
+    echo '<br>';
+    
+    echo '<pre>';
+        print_r($moto);
+    echo '</pre>';
+    echo '<hr />';
+
+    $carro->abrirTetoSolar();
+    echo '<br>';
+    $carro->acelerar();
+
+    echo '<hr />';
+
+    $moto->empinar();
 ?>
