ce9e094d4c3aa569cb03888ca9c2436694e4e46a
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-29 14:40:49 -0300

    update

--- POO_PHP/construtor_destrutor.php
+++ POO_PHP/construtor_destrutor.php
@@ -7,12 +7,26 @@
            $this->nome = $nome;
         }
 
+        function __destruct() {
+            echo 'Objeto removido';
+        }
+
         function __get($atributo) {
             return $this->$atributo;
+        }
+
+        function correr() {
+            return $this->__get('nome') . ' está correndo';
         }
 
    }
 
    $pessoa = new Pessoa('Léo');
    echo '<br> nome: ' . $pessoa-> __get('nome');
+   echo '<br>';
+   echo $pessoa->correr();
+
+   echo '<br>';
+   unset($pessoa); //chama o destruct
+
 ?>
