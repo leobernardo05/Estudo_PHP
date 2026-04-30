a847cc3ca336bf0b30c4eeacfa24df650492e263
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-30 15:17:05 -0300

    update

--- POO_PHP/atributos_e_metodos.php
+++ POO_PHP/atributos_e_metodos.php
@@ -1,14 +1,21 @@
 <?php
     class Exemplo {
-        public static $atributo1;
-        public $atributo2;
+        public static $atributo1 = 'Eu sou um atributo estático';
+        public $atributo2 = 'Eu sou um atributo normal';
 
         public static function metodo1(){
-
+            echo 'Eu sou um método estático';
         }
 
         public function metodo2(){
-            
+            echo 'Eu sou um método normal';
         }
     }
+
+    //$x = new Exemplo(); // não é necessario para metodos e atributos estaticos
+    echo Exemplo::$atributo1;
+    echo '<br>';
+    echo Exemplo::metodo1();
+
+    //echo Exemplo::metodo2(); //erro, pois não é metodo estatico
 ?>
