2b489ad148e36d807488e8acfcddeab545c3ae84
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-30 14:54:04 -0300

    update

--- POO_PHP/atributos_e_metodos.php
+++ POO_PHP/atributos_e_metodos.php
@@ -1,3 +1,14 @@
 <?php
-    
+    class Exemplo {
+        public static $atributo1;
+        public $atributo2;
+
+        public static function metodo1(){
+
+        }
+
+        public function metodo2(){
+            
+        }
+    }
 ?>
