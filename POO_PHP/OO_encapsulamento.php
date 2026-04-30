1bd8e90cbc105dc291567df80df42211c607df76
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-30 12:00:24 -0300

    update

--- POO_PHP/OO_encapsulamento.php
+++ POO_PHP/OO_encapsulamento.php
@@ -11,15 +11,54 @@
         public function __set($attr, $value){
             $this->$attr = $value;
         }
+
+        private function executarMania(){
+            echo 'Assobiar';
+        }
+
+        protected function responder(){
+            echo 'Oi';
+        }
+
+        public function executarAcao(){
+            $x = rand(1,10);  
+            
+            if($x >= 1 && $x <= 8) {
+                $this->executarMania();
+            } else {
+                $this->responder();
+            }
+        }
+    }
+
+    class Filho extends Pai {
+        public function getAtributo($attr){
+            return $this->$attr;
+        }
+
+        public function setAtributo($attr, $value){
+            $this->$attr = value;
+        }
     }
 
 
-    $pai = new Pai();
+    /*$pai = new Pai();
     // echo $pai->humor;
     echo $pai->sobrenome;
+    echo '<br>';
     echo $pai->sobrenome = 'Oliveira';
-    echo '<br>'
-    /*$pai->humor = 'Triste';
+    echo '<br>';
+    echo $pai->executarAcao();
+    $pai->humor = 'Triste';
     echo '<br>';
     echo $pai->humor;*/
+
+    $filho = new Filho();
+    echo '<pre>';
+        print_r($filho);
+    echo '</ pre>';
+    echo $filho->getAtributo('sobrenome');
+    $filho->setAtributo('humor', 'Triste');
+    echo '</ br>';
+    echo $filho->getAtributo('humor');
 ?>
