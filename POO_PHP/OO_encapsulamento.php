3e0cf17e71b485a30128d953c1576d0763214f93
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-30 10:29:30 -0300

    update

--- POO_PHP/OO_encapsulamento.php
+++ POO_PHP/OO_encapsulamento.php
@@ -1,3 +1,26 @@
 <?php
-    
+    class Pai {
+        private $nome = 'Jorge';
+        protected $sobrenome = 'Silva';
+        public $humor = 'Feliz';
+
+        public function getNome(){
+            return $this->nome;
+        }
+
+        public function setNome($value){
+            $this->nome = $value;
+        }
+    }
+
+
+    $pai = new Pai();
+    echo $pai->getNome();
+    $pai->setNome('Leo');
+    echo '<br>';
+    echo $pai
+
+    /*$pai->humor = 'Triste';
+    echo '<br>';
+    echo $pai->humor;*/
 ?>
