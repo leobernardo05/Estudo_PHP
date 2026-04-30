e0281c8b05ac6ac7b6ceab365eb5529a3d143a7e
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-04-30 15:37:57 -0300

    update interfaces.php

--- POO_PHP/interfaces.php
+++ POO_PHP/interfaces.php
@@ -37,4 +37,53 @@
 
     $x = new Geladeira();
     $y = new TV();
+
+    // --------------------------
+    interface MamiferoInterface {
+        public function respirar();
+    } 
+
+    interface TerrestreInterface {
+        public function andar();
+    }
+
+    interface AquaticoInterface {
+        public function nadar();
+    }
+
+    // interface cachorro
+    class Cachorro implements TerrestreInterface, MamiferoInterface {
+        public function andar(){
+            echo 'andando'
+        }    
+
+        public function respirar(){
+            echo 'respirando'
+        }
+    }
+
+    // interface baleia
+    class Baleia implements AquaticoInterface, MamiferoInterface {
+        public function respirar(){
+            echo 'respirando'
+        }
+
+        public function nadar(){
+            echo 'nadando'
+        }
+    }
+    // interface ornitorrinco que faz tudo, ou seja, implementa as 3 interfaces
+    class Ornitorrinco implements TerrestreInterface, AquaticoInterface, MamiferoInterface {
+        public function respirar(){
+            echo 'respirando'
+        }
+
+        public function andar(){
+            echo 'andando'
+        }
+
+        public function nadar(){
+            echo 'nadando'
+        }
+    }
 ?>
