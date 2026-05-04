23429c38ea10e13d53e0f444c64058d3e1d67f03
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-04 15:59:58 -0300

    update

--- POO_PHP/bibliotecas/lib2/lib2.php
+++ POO_PHP/bibliotecas/lib2/lib2.php
@@ -1,18 +1,21 @@
 <?php    
     namespace B;
 
-    class Cliente implements \A\CadastroInterface{
+    class Cliente implements CadastroInterface{
         public $nome = 'Santana';
+
+        public function __construct(){
+            echo '<pre>';
+                print_r(get_class_methods($this));
+            echo '</pre>';
+        }
+
         public function __get($attr){
             return $this->$attr;
         }
 
         public function excluir(){
             echo 'excluindo';
-        }
-
-        public function salvar(){
-            echo'excluindo';
         }
 
     }
