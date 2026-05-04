11ea3d03c9d181607b54a47294d2789c020708f2
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-04 16:11:45 -0300

    update

--- POO_PHP/namespace_parte2.php
+++ POO_PHP/namespace_parte2.php
@@ -2,9 +2,14 @@
     require "./bibliotecas/lib1/lib1.php";
     require "./bibliotecas/lib2/lib2.php";
 
-    use A\Cliente;
+    use A\Cliente as C1;
+    use B\Cliente as C2;
 
-    $c = new Cliente();
+    $c = new C1();
+    print_r($c);
+    echo $c-> __get('nome');
+
+    $c = new C2();
     print_r($c);
     echo $c-> __get('nome');
 ?>
