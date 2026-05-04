c4e7f608117c3fc5c08636d7fae671d018dc3c48
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-04 16:53:39 -0300

    update

--- POO_PHP/tratamento_erros.php
+++ POO_PHP/tratamento_erros.php
@@ -1,12 +1,24 @@
 <?php
     // tenha uma lógica
-    try {
+    try { //tente
         // tenha um lógic onde possa ocrrer um potencial erro(exceção)
+        echo '<h3> *** Try *** </h3>';
+        $sql = 'Select * from clientes';
+        mysql_query($sql); //Erro
+
+        if(!file_exists('requere_arquivo_a.php')){
+            // retorna verdadeiro
+            // throw new Exception();
+            throw new Error('Erroooooo')
+        }
+
+    } catch (Error $e){ //pegar
+        echo '<h3> *** Catch *** </h3>';
+        echo '<p style=calor: red;>' . $e . '</p'>
+        // armazenando esse erro em banco de dados
+    } finally { //finalmente
+        echo '<h3> *** Finally *** </h3>';
     }
 
     // tenha uma logica
-
-    try {
-        
-    }
 ?>
