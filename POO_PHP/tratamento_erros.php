0e6ebf057d062c9fe347f39db7ac996555687086
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-04 16:38:54 -0300

    Create tratamento_erros.php

---
+++ POO_PHP/tratamento_erros.php
@@ -0,0 +1,12 @@
+<?php
+    // tenha uma lógica
+    try {
+        // tenha um lógic onde possa ocrrer um potencial erro(exceção)
+    }
+
+    // tenha uma logica
+
+    try {
+        
+    }
+?>
