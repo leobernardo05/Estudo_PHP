2ac78651e387587264e573a972d76b5978dd8e13
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-04 16:09:09 -0300

    update

--- POO_PHP/namespace_parte2.php
+++ POO_PHP/namespace_parte2.php
@@ -1,3 +1,10 @@
 <?php
+    require "./bibliotecas/lib1/lib1.php";
+    require "./bibliotecas/lib2/lib2.php";
 
+    use A\Cliente;
+
+    $c = new Cliente();
+    print_r($c);
+    echo $c-> __get('nome');
 ?>
