3706514b5451004c4e5907da1ff8153c253893ff
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-19 14:39:17 -0300

    update

--- POO_PHP/argumentos-nomeados.php
+++ POO_PHP/argumentos-nomeados.php
@@ -8,6 +8,14 @@
 
     // convencional -> respeitando a ordem dos parametros
     sendEmail (
+        assunto: 'lbm3@discente.ifpe.edu.br'; //destinataria
+        destinatario: 'teste@teste.com.br'; //copia
+        mensagem: 'Argumentos Nomeados'; //assunto
+    );
+
+    echo '<hr>';
+
+    sendEmail (
         'lbm3@discente.ifpe.edu.br'; //destinataria
         'teste@teste.com.br'; //copia
         'Argumentos Nomeados'; //assunto
