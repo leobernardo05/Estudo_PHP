08faf87bc0e750f5efd41cba178dc5451dd032fa
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-19 15:05:57 -0300

    update

--- POO_PHP/promocao-propriedade-construtor.php
+++ POO_PHP/promocao-propriedade-construtor.php
@@ -9,7 +9,14 @@
         }
     }*/
 
-    $produto = new Produto('Smartphone', 1500);
+    class Produto {
+        public function __construct(
+            public string $nome = "",
+            public float $valor = 0
+        ) {}
+    }    
+
+    $produto = new Produto(valor: 1500, nome: 'Smartphone');
 
     echo "Produto: ".$produto->nome;
     echo "<br>";
