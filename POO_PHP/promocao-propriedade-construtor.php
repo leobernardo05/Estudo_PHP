76bedcaea8bac135c9114bd3adfce41af6c6ef65
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-19 14:48:35 -0300

    update
    nova notação do PHP 8

--- POO_PHP/promocao-propriedade-construtor.php
+++ POO_PHP/promocao-propriedade-construtor.php
@@ -1,5 +1,17 @@
 <?php
-    class Produto {
-        
-    }
+    /*class Produto {
+        public string $nome = "";
+        public float $valor = 0;
+
+        public function __construct($nome,$valor){
+            $this->nome = $nome;
+            this->valor = $valor;
+        }
+    }*/
+
+    $produto = new Produto('Smartphone', 1500);
+
+    echo "Produto: ".$produto->nome;
+    echo "<br>";
+    echo "Valor: ".$produto->valor;
 ?>
