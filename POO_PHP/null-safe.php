d1b3bc6307c44f32c17597b777dc5f1848105450
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-19 15:50:35 -0300

    update

--- POO_PHP/null-safe.php
+++ POO_PHP/null-safe.php
@@ -1,4 +1,22 @@
 <?php
-    
+    /*$objeto = null;
+
+    $objeto->total();
+
+    if(isset($objeto) && $objeto != null) {
+        $objeto->total();
+    }*/
+
+    //$objeto?->total();
+    class FolhaPagamento {
+        public function getTotalFuncionarios(){
+            return 25;
+        }
+    }
+
+    $folhaPagamento = new FolhaPagamento();
+
+    echo $folhaPagamento?->getTotalFuncionario(); //adiciona o null safe (?)
+
 
 ?>
