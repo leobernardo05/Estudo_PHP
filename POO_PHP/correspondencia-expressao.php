b61971535bc3928acc24c005e2706825a219f8be
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-19 15:13:46 -0300

    update

--- POO_PHP/correspondencia-expressao.php
+++ POO_PHP/correspondencia-expressao.php
@@ -14,4 +14,13 @@
 
     echo 'Resultado switch: '.$retornoSwitch;
     echo '<hr>';
+    
+    $retorno = match ($busca) {
+        '1' => 'Enconstrou o texto 1',
+        1 => 'Encontrou o number one',
+        default => 'Não encontrou'
+    }; // -> muito legal essa nova atualização do PHP 8+
+
+    echo "Resultado match: ".$retorno
+
 ?>
