b8486e5710dffe1386a68aec8f9f9a2e9a167304
Leonardo Bernardo Maciel <142163011+leobernardo05@users.noreply.github.com>
2026-05-19 15:10:33 -0300

    update

--- POO_PHP/correspondencia-expressao.php
+++ POO_PHP/correspondencia-expressao.php
@@ -1,3 +1,17 @@
 <?php
+    $busca = '2';
 
+    switch($busca){ //comparação por igualdade/equalidade
+        case '1':
+                $retornoSwitch = 'Encontrou';
+                break;
+        case '2':
+                $retornoSwitch = 'Encontrou'
+                break;
+        default:
+                $retornoSwitch = 'NÃO encontrou';
+    }
+
+    echo 'Resultado switch: '.$retornoSwitch;
+    echo '<hr>';
 ?>
